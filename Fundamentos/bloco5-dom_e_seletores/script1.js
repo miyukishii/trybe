713f5dbeb8f9ac1af8b397b21d445fe589aa049c
Miyuki Ishii <miyukisabrina@gmail.com>
2022-04-13 19:42:04 -0300

    adicionando exercícios do dia 1 bloco 5

---
+++ Fundamentos/bloco5-dom_e_seletores/script1.js
@@ -0,0 +1,14 @@
+let emergencyTasks = document.getElementsByClassName("emergency-tasks")[0];
+emergencyTasks.style.background = "#f6bcf6";
+
+let noEmergencyTasks = document.getElementsByClassName("no-emergency-tasks")[0];
+noEmergencyTasks.style.background = "#f0d68f";
+
+let footer = document.getElementById("footer-container");
+footer.style.background = "#29a329";
+
+let subTitles = document.querySelectorAll("h3")[0];
+subTitles.style.background = "#ff6666";
+
+let subTitles1 = document.getElementsByTagName("h3")[2];
+subTitles1.style.background = "blue";
