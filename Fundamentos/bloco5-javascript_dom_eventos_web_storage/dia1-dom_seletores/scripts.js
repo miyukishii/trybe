b98c78aad564af254f263b0d2b56f961e942eecb
Miyuki Ishii <miyukisabrina@gmail.com>
2022-04-14 10:57:24 -0300

    movendo os arquivos para o dia 1

---
+++ Fundamentos/bloco5-javascript_dom_eventos_web_storage/dia1-dom_seletores/scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("page-title").innerText = " Orgulho e Preconceito";
+document.getElementById("second-paragraph").innerText = "Baseado no romance de Jane Austen";
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+document.getElementById("subtitle").innerText = "Romance atemporal";
+document.getElementsByClassName("atores")[0].innerText = "Lizzy";
