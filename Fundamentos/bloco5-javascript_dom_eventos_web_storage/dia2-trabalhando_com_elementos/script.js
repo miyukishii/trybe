3129883886edd0f68133478eff8a92ac0f9c2f69
Miyuki Ishii <miyukisabrina@gmail.com>
2022-04-14 19:56:34 -0300

    adicionando exercícios incompletos do dia 2 bloco 5

---
+++ Fundamentos/bloco5-javascript_dom_eventos_web_storage/dia2-trabalhando_com_elementos/script.js
@@ -0,0 +1,7 @@
+let element = document.getElementById("elementoOndeVoceEsta");
+document.getElementById("elementoOndeVoceEsta").parentNode;
+element.innerText = "pai";
+element.style.color = "#008B8B";
+
+let writeText = document.getElementById("primeiroFilhoDoFilho");
+writeText.innerText = "Primeiro filho do filho";
