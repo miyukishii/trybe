c07f21942daebc635e0104ef9ffa534649ce17ec
Miyuki Ishii <miyukisabrina@gmail.com>
2022-04-20 14:43:03 -0300

    adicionando exercícios de revisão

--- Fundamentos/bloco4-introducao_a_javascript/dia4-objetos_e_funcoes/revisao.js
+++ Fundamentos/bloco4-introducao_a_javascript/dia4-objetos_e_funcoes/revisao.js
@@ -130,3 +130,111 @@ if(a>b){
 //   return false;
 // }
 // console.log(even([1,3,4]));
+
+// Exercício 9) Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+
+// function odd(array){
+// for (let index in array) {
+//     if(array[index] %2 === 1){
+//        return true;
+//     }
+//   }
+//   return false;
+// }
+// console.log(odd([2,2,1]));
+
+// Exercício 10) Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+
+// function profit(cost,priceOfSale){
+// const costTax = cost*20/100;
+// let numOfSales = 1000;
+// let profitValue = 0;
+//     if(cost<0 || priceOfSale < 0){
+//         console.log("Error!");
+//     }else{
+//        profitValue = (priceOfSale - costTax)*numOfSales;
+//     }
+//     return profitValue;
+// }
+// console.log(profit(100,150));
+
+// 11)
+// let salary = 3000;
+// function netSalary (){
+//     if(salary === 1556.94 || salary <1556.94) {
+//         let aliquota = 8;
+//     }else if(salary === 1556.95 || salary < 1556.95) {
+//         aliquota = 9;
+//     }
+//     }
+// }
+
+// 12) Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// console.log(numbers);
+
+// 13) Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// let resultado = 0;
+// for(let index in numbers){
+//      resultado += numbers[index];
+// }
+// console.log(resultado);
+
+// 14) Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+// let medArit = resultado/numbers.length;
+// console.log(medArit);
+
+// 15) Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+// if(medArit > 20) {
+//     console.log("valor maior que 20");
+// } else {
+//     console.log("valor menor ou igual a 20");
+// }
+
+// 16) Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+// let maiorValor = numbers[0];
+// for(index in numbers) {
+//     if(numbers[index] > maiorValor) {
+//         maiorValor = numbers[index];
+//     }
+// }
+// console.log(maiorValor);
+
+// 17) Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+// let oddNum = 0;
+// let result = 0;
+// for(index in numbers){
+//     if(numbers[index]%2 === 1){
+//         oddNum = oddNum + 1;
+//         result = oddNum;
+//     }
+// }
+// if(oddNum < 1){
+//     result = "nenhum valor ímpar encontrado";
+// }
+//  console.log(result);
+
+// 18) Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+// let menorValor = numbers[0];
+// for(index in numbers) {
+//     if (numbers[index] < menorValor) {
+//         menorValor = numbers[index];
+//     }
+// }
+// console.log(menorValor);
+
+// 19) Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+// let arrayNum = [];
+// for(i = 1; i < 26; i += 1){
+//     arrayNum.push(i);
+// }
+// console.log(arrayNum);
+
+// 20) Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+// let newArray = [];
+// for (let index in arrayNum) {
+//     newArray.push(arrayNum[index]/2);
+// }
+// console.log(newArray);
