760b1b5162dcc2ed2b4ecc51aeeeb367d384eab6
Miyuki Ishii <miyukisabrina@gmail.com>
2022-06-20 11:58:00 -0300

    Anotações do bloco 10 dia 1

---
+++ Front-end/bloco10-introducao_a_react/dia1-hello_world/parafixar.js
@@ -0,0 +1,4 @@
+// 1 - Para fixar, crie um elemento JSX que recebe o valor "Hello, JSX" de uma constante chamada textJSX, e o incorpore em uma tag h1.
+
+const textJSX = 'Hello, JSC';
+const element = <h1>{textJSX}</h1>;
